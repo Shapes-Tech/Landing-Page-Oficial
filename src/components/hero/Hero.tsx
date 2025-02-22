9564b42c29297fbc277a7b39b5995a95aa4ab725
nachof948 <nachof948@gmail.com>
2025-02-22 12:05:33 -0300

    fix(hero): fix the brightness of button

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,15 +6,18 @@ import { useSelector } from 'react-redux'
 import { RootState } from '../../store'
 export const Hero: React.FC = () => {
   const currentLanguage = useSelector((state: RootState) => state.language.currentLanguage)
+  const currentColor = useSelector((state: RootState) => state.color.mode)
   const t = translations[currentLanguage]
+  const titleColor = currentColor === 'dark' ? 'text-white opacity-40' : 'text-black opacity-90' 
+  const descriptionColor = currentColor === 'dark' ? 'text-white' : 'text-black' 
   return (
     <section className='flex flex-col'>
       <NavBar />
       <div className="flex items-start gap-14">
         <img src={shapes} />
         <div className="flex flex-col">
-          <h1 className='font-quicksand text-white opacity-40 text-[10vw] font-extralight'>{t.hero.title}</h1>
-          <p className='font-quicksand text-white text-3xl w-[90%] font-extralight'>{t.hero.description}</p>
+          <h1 className={`font-quicksand ${titleColor} text-[10vw] font-extralight`}>{t.hero.title}</h1>
+          <p className={`font-quicksand ${descriptionColor} text-3xl w-[90%] font-extralight`}>{t.hero.description}</p>
           <button className='button-hero animate-button font-medium mt-7 cursor-pointer'>{t.hero.button}</button>
         </div>
       </div>
