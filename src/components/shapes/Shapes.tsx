c1d7dfacef19d013ce0c96f8c5cfeeb1cc32eaa9
nachof948 <nachof948@gmail.com>
2025-03-14 10:21:37 -0300

    fix(shapes): add a mode responsive mobile

--- src/components/shapes/Shapes.tsx
+++ src/components/shapes/Shapes.tsx
@@ -4,10 +4,10 @@ import Square from '../../assets/Square.gif'
 import Triangle from '../../assets/Triangle.gif'
 const Shapes: React.FC = () => {
   return (
-    <section className='flex justify-center gap-10 items-center w-[80%] mx-auto mt-10'>
-      <img className=' w-32 h-32 shapes-img-lg:w-48 shapes-img-lg:h-48' src={Square} alt="" />
-      <img className=' w-32 h-32 shapes-img-lg:w-48 shapes-img-lg:h-48' src={Circle} alt="" />
-      <img className=' w-32 h-32 shapes-img-lg:w-48 shapes-img-lg:h-48' src={Triangle} alt="" />
+    <section className='flex justify-center gap-4 shapes-img-md:gap-10 items-center w-[80%] mx-auto mt-10'>
+      <img className=' w-20 h-20 shapes-img-md:w-32 shapes-img-md:h-32 shapes-img-lg:w-48 shapes-img-lg:h-48' src={Square} alt="" />
+      <img className=' w-20 h-20 shapes-img-md:w-32 shapes-img-md:h-32 shapes-img-lg:w-48 shapes-img-lg:h-48' src={Circle} alt="" />
+      <img className=' w-20 h-20 shapes-img-md:w-32 shapes-img-md:h-32 shapes-img-lg:w-48 shapes-img-lg:h-48' src={Triangle} alt="" />
     </section>
   )
 }
